7df93bb689c5bc7b16906e18a19dddd6a0157321
Your Name <you@example.com>
2022-04-25 21:17:17 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -49,9 +49,9 @@ class App extends React.Component {
         let output = data.label;
         let color = "white"
         switch(output) {
-          case "positive": color = "black";break;
-          case "negative": color = "black";break;
-          default: color = "black";
+          case "positive": color = "green";break;
+          case "negative": color = "red";break;
+          default: color = "yellow";
         }
         output = <div style={{color:color,fontSize:20}}>{output}</div>
         this.setState({sentimentOutput:output});
@@ -63,12 +63,13 @@ class App extends React.Component {
     this.setState({sentiment:false});
     let url = ".";
     let mode = this.state.mode
-    url = url+"/" + mode + "/emotion?"+ mode + "="+document.getElementById("textinput").value;
-
+    url = url+"/" + mode + "/sentiment?"+ mode + "="+document.getElementById("textinput").value;
+    //this.setState({sentimentOutput:url});
     fetch(url).then((response)=>{
-      response.json().then((data)=>{
-      this.setState({sentimentOutput:<EmotionTable emotions={data}/>});
-  })})  ;
+        response.json().then((data)=>{
+            this.setState({sentimentOutput:<EmotionTable emotions={data}/>})
+        });
+    });
   }
   
 
--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -4,14 +4,14 @@ import './bootstrap.min.css';
 class EmotionTable extends React.Component {
     render() {
       //Returns the emotions as an HTML table
-      return (  
+      return (   
         <div>
           <table className="table table-bordered">
             <tbody>
             {
-              /*Write code to use the .map method that you worked on in the 
-              Hands-on React lab to extract the emotions. If you are stuck,
-              please click the instructions to see how to implement a map*/
+                this.state.emotions.map((emotionscore)=>{
+                    return <tr><td>emotionscore[0]</td><td>emotionscore[1]</td></tr>
+                })
             }
             </tbody>
           </table>
